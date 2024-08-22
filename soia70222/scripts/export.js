f4b2948f459d6b70647c52b4f8148f2776d9dfa8
Lucas dos Santos <lucas.santos@atlasinovacoes.com.br>
2024-08-22 10:26:15 -0300

    .

--- soia70222/scripts/export.js
+++ soia70222/scripts/export.js
@@ -43,8 +43,8 @@ function getCurrentDate() {
   const month = String(currentDate.getMonth() + 1).padStart(2, '0');
   const day = String(currentDate.getDate()).padStart(2, '0');
   
-  const hours = String(agora.getHours()).padStart(2, '0');
-  const minutes = String(agora.getMinutes()).padStart(2, '0');
+  const hours = String(currentDate.getHours()).padStart(2, '0');
+  const minutes = String(currentDate.getMinutes()).padStart(2, '0');
 
   return `${year}_${month}_${day}_${hours}_${minutes}`;
 }
